10fa7fb49ace1d0f6bdae516578bab4602e7a956
Marc Ignacio <padi@users.noreply.github.com>
2016-02-28 04:30:39 +0800

    Add Scrape#save_movie

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Scrape
   attr_accessor :title, :hotness, :image_url, :rating, :director, :genre, :release_date, :runtime, :synopsis, :failure
 
@@ -13,10 +15,28 @@ class Scrape
       self.director = doc.at("//span[@itemprop='name']").text
       self.genre = doc.at("//span[@itemprop='genre']").text
       self.runtime = doc.at("//time[@itemprop='duration']").text
-      self.synopsis =  doc.at("#movieSynopsis").text
+      s = doc.at("#movieSynopsis").text
+      if ! s.valid_encoding?
+        s = s.encode("UTF-16be", :invalid=>:replace, :replace=>"?").encode('UTF-8')
+      end
+      self.synopsis = s
     rescue => e
       self.failure = "Something went wrong with the scrape: #{e.message}"
     end
   end
 
+  def save_movie
+    movie = Movie.new(
+      title: self.title,
+      hotness: self.hotness,
+      image_url: self.image_url,
+      rating: self.rating,
+      director: self.director,
+      genre: self.genre,
+      release_date: self.release_date,
+      runtime: self.runtime,
+      synopsis: self.synopsis,
+    )
+    movie.save
+  end
 end
